f5f399b6ff2fd937e090af230029faad506803a8
SAIRAM N <ramphilo@gmail.com>
2024-06-27 23:32:47 +0530

    Added Easy Java
    Added Easy Java

---
+++ Easy/338. Counting Bits.java
@@ -0,0 +1,45 @@
+// 338. Counting Bits
+// https://leetcode.com/problems/counting-bits/description
+// Hint
+// Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+// Example 1:
+
+// Input: n = 2
+// Output: [0,1,1]
+// Explanation:
+// 0 --> 0
+// 1 --> 1
+// 2 --> 10
+// Example 2:
+
+// Input: n = 5
+// Output: [0,1,1,2,1,2]
+// Explanation:
+// 0 --> 0
+// 1 --> 1
+// 2 --> 10
+// 3 --> 11
+// 4 --> 100
+// 5 --> 101
+
+// Constraints:
+
+// 0 <= n <= 105
+
+// Follow up:
+
+// It is very easy to come up with a solution with a runtime of O(n log n). Can you do it in linear time O(n) and possibly in a single pass?
+// Can you do it without using any built-in function (i.e., like __builtin_popcount in C++)?
+
+// Solution : 
+
+class Solution {
+    public int[] countBits(int n) {
+        int[] res = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            res[i] = res[i >> 1] + (i & 1);
+        }
+        return res;
+    }
+}
