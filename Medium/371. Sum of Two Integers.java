0321c5d63a4c36b6567ed4b9a74495ead9ebd882
SAIRAM N <ramphilo@gmail.com>
2024-07-07 20:08:00 +0530

    Added Medium Java
    Added Medium Java

---
+++ Medium/371. Sum of Two Integers.java
@@ -0,0 +1,30 @@
+// 371. Sum of Two Integers
+// https://leetcode.com/problems/sum-of-two-integers/description/
+// Companies
+// Given two integers a and b, return the sum of the two integers without using the operators + and -.
+
+// Example 1:
+
+// Input: a = 1, b = 2
+// Output: 3
+// Example 2:
+
+// Input: a = 2, b = 3
+// Output: 5
+
+// Constraints:
+
+// -1000 <= a, b <= 1000
+
+// Solution : 
+
+class Solution {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int carry = (a & b) << 1;
+            a = a ^ b;
+            b = carry;
+        }
+        return a;
+    }
+}
