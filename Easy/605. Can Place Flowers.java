e9f74e4aff9e4ef361c72614fe1f1921004d147d
SAIRAM N <ramphilo@gmail.com>
2024-07-20 16:05:18 +0530

    Added Easy Java
    Added Easy Java

---
+++ Easy/605. Can Place Flowers.java
@@ -0,0 +1,40 @@
+// 605. Can Place Flowers
+// https://leetcode.com/problems/can-place-flowers/description/
+// You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+// Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+// Example 1:
+
+// Input: flowerbed = [1,0,0,0,1], n = 1
+// Output: true
+// Example 2:
+
+// Input: flowerbed = [1,0,0,0,1], n = 2
+// Output: false
+
+// Constraints:
+
+// 1 <= flowerbed.length <= 2 * 104
+// flowerbed[i] is 0 or 1.
+// There are no two adjacent flowers in flowerbed.
+// 0 <= n <= flowerbed.length
+
+// Solution : 
+
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int l = flowerbed.length;
+        for (int i = 0; i < l && n > 0; i++) {
+            if (flowerbed[i] == 0) {
+                boolean first = (i == 0) || (flowerbed[i - 1] == 0);
+                boolean second = (i == l - 1) || (flowerbed[i + 1] == 0);
+                if (first && second) {
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            }
+        }
+        return n == 0 ? true : false;
+    }
+}
