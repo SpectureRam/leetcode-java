32cf74b1e8a0e9493f3030884bc5ca349203e8ac
SAIRAM N <ramphilo@gmail.com>
2024-07-22 22:33:28 +0530

    Added Easy Java
    Added Easy Java

---
+++ Easy/448. Find All Numbers Disappeared in an Array.java
@@ -0,0 +1,41 @@
+// 448. Find All Numbers Disappeared in an Array
+// https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/description/
+// Hint
+// Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+// Example 1:
+
+// Input: nums = [4,3,2,7,8,2,3,1]
+// Output: [5,6]
+// Example 2:
+
+// Input: nums = [1,1]
+// Output: [2]
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 105
+// 1 <= nums[i] <= n
+
+// Follow up: Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+// Solution : 
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = nums[index] * -1;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                list.add(i + 1);
+            }
+        }
+        return list;
+    }
+}
