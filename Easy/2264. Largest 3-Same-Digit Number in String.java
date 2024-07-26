63829b322bcd88e9b5337d309b55a51f8fec3127
SAIRAM N <ramphilo@gmail.com>
2024-07-26 23:20:13 +0530

    Added Easy Java
    Added Easy Java

---
+++ Easy/2264. Largest 3-Same-Digit Number in String.java
@@ -0,0 +1,47 @@
+// 2264. Largest 3-Same-Digit Number in String
+// https://leetcode.com/problems/largest-3-same-digit-number-in-string/description/
+// Hint
+// You are given a string num representing a large integer. An integer is good if it meets the following conditions:
+
+// It is a substring of num with length 3.
+// It consists of only one unique digit.
+// Return the maximum good integer as a string or an empty string "" if no such integer exists.
+
+// Note:
+
+// A substring is a contiguous sequence of characters within a string.
+// There may be leading zeroes in num or a good integer.
+
+// Example 1:
+
+// Input: num = "6777133339"
+// Output: "777"
+// Explanation: There are two distinct good integers: "777" and "333".
+// "777" is the largest, so we return "777".
+// Example 2:
+
+// Input: num = "2300019"
+// Output: "000"
+// Explanation: "000" is the only good integer.
+// Example 3:
+
+// Input: num = "42352338"
+// Output: ""
+// Explanation: No substring of length 3 consists of only one unique digit. Therefore, there are no good integers.
+
+// Constraints:
+
+// 3 <= num.length <= 1000
+// num only consists of digits.
+
+// Solution : 
+
+class Solution {
+for(int i = 9; i >= 0; i--){
+    String s = String.valueOf(i).repeat(3);
+    if(num.contains(s)){
+        return s;
+    }
+}
+return "";
+}
