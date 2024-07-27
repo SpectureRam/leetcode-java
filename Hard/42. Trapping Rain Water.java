07325ee1f0227a1f76aa2b8f3f6b6817a7b439b4
SAIRAM N <ramphilo@gmail.com>
2024-07-27 23:59:06 +0530

    Added Hard Java
    Added Hard Java

--- Hard/42. Trapping Rain Water.java
+++ Hard/42. Trapping Rain Water.java
@@ -1,6 +1,6 @@
 // 42. Trapping Rain Water
 // https://leetcode.com/problems/trapping-rain-water/description/
-// Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+// Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap  after raining.
 
 // Example 1:
 
