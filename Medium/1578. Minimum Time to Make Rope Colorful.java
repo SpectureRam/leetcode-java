cb8ece5f76964cc197a83608d4afe913d68b5063
SAIRAM N <ramphilo@gmail.com>
2024-07-31 21:50:14 +0530

    Added Medium Java
    Added Medium Java

---
+++ Medium/1578. Minimum Time to Make Rope Colorful.java
@@ -0,0 +1,54 @@
+// 1578. Minimum Time to Make Rope Colorful
+// https://leetcode.com/problems/minimum-time-to-make-rope-colorful/description/
+// Alice has n balloons arranged on a rope. You are given a 0-indexed string colors where colors[i] is the color of the ith balloon.
+
+// Alice wants the rope to be colorful. She does not want two consecutive balloons to be of the same color, so she asks Bob for help. Bob can remove some balloons from the rope to make it colorful. You are given a 0-indexed integer array neededTime where neededTime[i] is the time (in seconds) that Bob needs to remove the ith balloon from the rope.
+
+// Return the minimum time Bob needs to make the rope colorful.
+
+// Example 1:
+
+// Input: colors = "abaac", neededTime = [1,2,3,4,5]
+// Output: 3
+// Explanation: In the above image, 'a' is blue, 'b' is red, and 'c' is green.
+// Bob can remove the blue balloon at index 2. This takes 3 seconds.
+// There are no longer two consecutive balloons of the same color. Total time = 3.
+// Example 2:
+
+// Input: colors = "abc", neededTime = [1,2,3]
+// Output: 0
+// Explanation: The rope is already colorful. Bob does not need to remove any balloons from the rope.
+// Example 3:
+
+// Input: colors = "aabaa", neededTime = [1,2,3,4,1]
+// Output: 2
+// Explanation: Bob will remove the balloons at indices 0 and 4. Each balloons takes 1 second to remove.
+// There are no longer two consecutive balloons of the same color. Total time = 1 + 1 = 2.
+
+// Constraints:
+
+// n == colors.length == neededTime.length
+// 1 <= n <= 105
+// 1 <= neededTime[i] <= 104
+// colors contains only lowercase English letters.
+
+// Solution : 
+
+class Solution {
+    public int minCost(String colors, int[] neededTime) {
+        int res = 0, left = 0, n = colors.length();
+        for (int right = 1; right < n; right++) {
+            if (colors.charAt(left) == colors.charAt(right)) {
+                if (neededTime[left] < neededTime[right]) {
+                    res += neededTime[left];
+                    left = right;
+                } else {
+                    res += neededTime[right];
+                }
+            } else {
+                left = right;
+            }
+        }
+        return res;
+    }
+}
