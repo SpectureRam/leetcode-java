d7f86298b149a965261c2dddc424812b0c3bbd2e
SAIRAM N <ramphilo@gmail.com>
2024-08-08 19:22:17 +0530

    Added Easy Java
    Added Easy Java

---
+++ Easy/3005. Count Elements With Maximum Frequency.java
@@ -0,0 +1,49 @@
+// 3005. Count Elements With Maximum Frequency
+// https://leetcode.com/problems/count-elements-with-maximum-frequency/description/
+// Hint
+// You are given an array nums consisting of positive integers.
+
+// Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
+
+// The frequency of an element is the number of occurrences of that element in the array.
+
+// Example 1:
+
+// Input: nums = [1,2,2,3,1,4]
+// Output: 4
+// Explanation: The elements 1 and 2 have a frequency of 2 which is the maximum frequency in the array.
+// So the number of elements in the array with maximum frequency is 4.
+// Example 2:
+
+// Input: nums = [1,2,3,4,5]
+// Output: 5
+// Explanation: All elements of the array have a frequency of 1 which is the maximum.
+// So the number of elements in the array with maximum frequency is 5.
+
+// Constraints:
+
+// 1 <= nums.length <= 100
+// 1 <= nums[i] <= 100
+
+// Solution : 
+
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+        int count = 0, max = 0, sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int freq : map.values()) {
+            if (freq > max) {
+                max = freq;
+            }
+        }
+        for (int num : nums) {
+            if (map.get(num) == max) {
+                sum++;
+            }
+        }
+        return sum;
+    }
+}
